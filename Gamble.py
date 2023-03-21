b800ece07f17001337f12d252fc6132f2b6f91e5
BrianTanJunWei <briantan977@gmail.com>
2023-03-21 23:05:57 +0800

    Local provably-fair works game
    Updated it

--- Gamble.py
+++ Gamble.py
@@ -4,38 +4,37 @@ import main
 
 # Return user input
 
-
 def server_seed():
     #generate a random sha256 hash each time player rerun the code
 
-    #can be a randomized hashed value or *see line 15
-    # prehashServerseed = str(random.getrandbits(256))
-    # print("This is a prehash value: " + prehashServerseed)
+    #for testing purpose
+   # prehashServerseed = "85943002341619080928654322842274335146257750056392232559940811297469147320250" #can either be a fixed hashed value
+    #print("This is a prehash value: " + prehashServerseed)
 
-    prehashServerseed = "85943002341619080928654322842274335146257750056392232559940811297469147320250" #can either be a fixed hashed value
+    # can be a randomized hashed value or *see line 15
+    prehashServerseed = str(random.getrandbits(256))
     print("This is a prehash value: " + prehashServerseed)
 
     #hashed serverseed
     server_seed = hashlib.sha256(prehashServerseed.encode('utf-8')).hexdigest()
     print("This is the server seed: " + server_seed)
 
-
+    #This part is for user to check if the server seed had been modified, usually users will check the server seed online
     expected_server_seed = hashlib.sha256(prehashServerseed.encode('utf-8')).hexdigest()
 
-# XOR the prehash and hashedserver to verify if user taper with it
-    if int(server_seed, 16) ^ int(expected_server_seed, 16) != 0:
-        return False
-    else:
-        print("Seed is not tapered with")
 
     #for testing purpose
-    server_seed = "85943002341619080928654322842274335146257750056392232559940811297469147320250aa"
+    #server_seed = "85943002341619080928654322842274335146257750056392232559940811297469147320250"
 
+    # XOR the prehash and hashedserver to verify if user taper with it
     if int(server_seed, 16) ^ int(expected_server_seed, 16) != 0:
-        print("WTF BRO?")
+       print("Seed is tapered with")
     else:
         print("Seed is not tapered with")
+
+    return server_seed
     #test until here
+
 
 def flip_coin(player_seed, server_seed):
     # Combine player seed and server seed to generate a unique seed for this round
@@ -52,35 +51,63 @@ def flip_coin(player_seed, server_seed):
     return result, combined_seed
 
 
-def verify_result(player_seed, server_seed, result, combined_seed):
-    # Combine player seed and server seed to generate the same unique seed used to generate the result
-    expected_seed = player_seed + server_seed
+# def verify_result(player_seed, server_seed, result, combined_seed):
+#     # Combine player seed and server seed to generate the same unique seed used to generate the result
+#     expected_seed = player_seed + server_seed
+#
+#     # Generate a random number using the expected seed
+#     random.seed(expected_seed)
+#     rand_num = random.random()
+#
+#     # Determine the expected result based on the random number
+#     expected_result = "heads" if rand_num < 0.5 else "tails"
+#
+#     # Verify that the expected result matches the actual result
+#     if expected_result == result:
+#         # Verify that the combined seed matches the expected seed
+#         if combined_seed == expected_seed:
+#             return True
+#     return False
 
-    # Generate a random number using the expected seed
-    random.seed(expected_seed)
-    rand_num = random.random()
 
-    # Determine the expected result based on the random number
-    expected_result = "heads" if rand_num < 0.5 else "tails"
+def player_seed():
+    #generate a random sha256 hash each time player rerun the code
 
-    # Verify that the expected result matches the actual result
-    if expected_result == result:
-        # Verify that the combined seed matches the expected seed
-        if combined_seed == expected_seed:
-            return True
-    return False
+    # can be a randomized hashed value or *see line 15
+    #prehashPlayerseed = str(random.getrandbits(256))
+    #print("This is a prehash user seed: " + prehashPlayerseed)
 
+    prehashPlayerseed = "d9550b6a19dd31dc774318cb70568d1ff6c0455417aab774ea38632f0448955b"
+    #hashed player seed
+    player_seed = hashlib.sha256(prehashPlayerseed.encode('utf-8')).hexdigest()
+    #print("This is the user seed: " + player_seed)
 
 
-player_seed = "mysecretseed"
+    #This part is for user to check if the server seed had been modified, usually users will check the server seed online
+    expected_Player_seed = hashlib.sha256(prehashPlayerseed.encode('utf-8')).hexdigest()
+
+
+    # XOR the prehash and hashedserver to verify if user taper with it
+    if int(player_seed, 16) ^ int(expected_Player_seed, 16) != 0:
+       print("Player Seed is tapered with")
+    else:
+       print("Player Seed is not tapered with")
+       print(player_seed)
+       return player_seed
+
+
+
+
+#player_seed = "mysecretseed"
 
 server_seed = str(server_seed())
-result, combined_seed = flip_coin(player_seed, server_seed)
+player_seed = str(player_seed())
+
+result = flip_coin(player_seed, server_seed)
 print("Result:", result)
 
-print("Combined seed:", combined_seed)
-verified = verify_result(player_seed, server_seed, result, combined_seed)
-print("Result verified:", verified)
+#print("Combined seed:", combined_seed)
 
-
+# verified = verify_result(player_seed, server_seed, result, combined_seed)
+#print("Result verified:", verified)
 
