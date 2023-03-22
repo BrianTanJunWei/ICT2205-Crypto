de588817d8d6dfedeef52ee5a4df042bbc3d9c99
Excelic <ngyx31@gmail.com>
2023-03-22 20:57:28 +0800

    testing TLS connection

--- client.py
+++ client.py
@@ -1,15 +1,25 @@
 import socket
 import sys
+import ssl 
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_verify_locations('new.pem')
+
+server_address = ('localhost', 10000)
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+c_soc=context.wrap_socket(sock,server_hostname='localhost')
+
+
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print('connecting to {} port {}'.format(*server_address))
-sock.connect(server_address)
+print(f'connecting to {server_address[0]} port {server_address[1]}')
+c_soc.connect(server_address)
 try:
     # Send data
     message = b'This is the message. It will be repeated.'
-    print('sending {!r}'.format(message))
+    print(f'sending {message!r}')
     sock.sendall(message)
     # Look for the response
     amount_received = 0
@@ -17,7 +27,7 @@ try:
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
-        print('received {!r}'.format(data))
+        print(f'received {data!r}')
 finally:
     print('closing socket')
     sock.close()
--- server.py
+++ server.py
@@ -1,28 +1,36 @@
 import socket
 import sys
+import ssl 
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('new.pem','private.key')
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
+print(f'starting up on {server_address[0]} port {server_address[1]}')
 sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(2) #listen to 2 connection, client A and client B
+
+s_sock = context.wrap_socket(sock, server_side=True)
+
 while True:
     # Wait for a connection
     print('waiting for a connection')
-    connection, client_address = sock.accept()
+    connection, client_address = s_sock.accept()
     try:
         print('connection from', client_address)
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print('received {!r}'.format(data))
+            print(f'received {data!r}')
             if data:
                 print('sending data back to the client')
                 connection.sendall(data)
             else:
-                print('no data from', client_address)
+                print('no data from ', client_address)
             break
     finally:
     # Clean up the connection
