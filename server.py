1f85685ea13ba54291a6c392af1e7d606c1a85da
Nithit <nithitlim1996@gmail.com>
2023-03-23 21:52:07 +0800

    Converted to json
    Touchup needed but manage to make a json

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import sys
+import json
 
 #params
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -24,6 +25,7 @@ def handle_client(connection, client_address):
     file_name = f"{client_address[0]}_{client_address[1]}.txt"
     with open(file_name, "w") as f:
         f.write("Hello, world!")
+
         while connected:
             data = connection.recv(BYTE_RECV)
             if data.decode().strip() == DISCONNECT_MESSAGE:
@@ -31,10 +33,35 @@ def handle_client(connection, client_address):
                 connection.close()
                 f.close()
                 print(f"[DISCONNECTED] {client_address} disconnected. File written: {file_name}")
+                convert_text_to_json(file_name, "json_log.json") 
             elif len(data) != 0:
                 f.write(data.decode())
 
+def convert_text_to_json(input_file, output_file):
+    # Open the input file and read the lines
+    with open(input_file, "r") as f:
+        lines = f.readlines()
 
+    # Create a list to hold the dictionaries
+    data = []
+
+    # Iterate over the lines and parse them into dictionaries
+    for line in lines:
+        # Parse the line into fields
+        fields = line.strip().split("|")
+
+        # Create a dictionary with the fields as keys and values
+        d = {
+            "Client Seed": fields[0],
+            # Add more fields as needed
+        }
+
+        # Append the dictionary to the list
+        data.append(d)
+
+    # Write the list of dictionaries to the output file as JSON
+    with open(output_file, "w") as f:
+        json.dump(data, f, indent=4)
                 
 
 def start():
