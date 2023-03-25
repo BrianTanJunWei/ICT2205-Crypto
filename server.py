49b8c8167bc5711e1ff1137303baec0e13d11691
Nithit <nithitlim1996@gmail.com>
2023-03-25 20:51:55 +0800

    Updates for making a text file
    Text file seem to only write when disconnect

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 import socket
 import threading
 import sys
-<<<<<<< Updated upstream
 import json
-=======
 import main
 import time
 import FileHandler as FH
->>>>>>> Stashed changes
 import main
 import time
 
@@ -40,59 +37,67 @@ def handle_client(connection, client_address):
     changeSeed = "No need change"
     client_seed = "GoingToBeChanged"
     server_seed = "GoingToBeChanged"
+#Open file
+    file_name = f"{client_address[0]}_{client_address[1]}.txt"
+    with open(file_name, "a") as f:
 
+        while connected:
+            data = connection.recv(BYTE_RECV).decode().strip()
+            if data == DISCONNECT_MESSAGE:
+                connected = False
+                connection.close()
+                print(f"[DISCONNECTED] {client_address} disconnected.")
+                FH.convert_text_to_json(file_name, "Result.json")# having issues
+            elif len(data) != 0:
+                if data == "1":
+                    client_answer = ""
+                    server_answer = ""
 
-    while connected:
-        data = connection.recv(BYTE_RECV).decode().strip()
-        if data == DISCONNECT_MESSAGE:
-            connected = False
-            connection.close()
-            print(f"[DISCONNECTED] {client_address} disconnected.")
-        elif len(data) != 0:
-            if data == "1":
-                client_answer = ""
-                server_answer = ""
-
-                nonce = main.get_nonce(nonce, status)[0]
-                server_seed = main.generate_server_seed(server_seed)
-                client_seed, changeSeed = main.generate_client_seed(client_seed, changeSeed)
-                roll, server_seed, client_seed = main.get_roll_and_seeds(nonce, server_seed, client_seed)
-                connection.sendall(f"Client seed: {client_seed}\nNonce: {nonce}\n".encode())
-                # print(f'\nRoll for nonce {nonce} is {roll}')data = connection.recv(BYTE_RECV).decode().strip()
-                status = False
-                data = connection.recv(BYTE_RECV).decode().strip()
-                if data =="heads" or data =="tails":
-                    client_answer = data
+                    nonce = main.get_nonce(nonce, status)[0]
+                    server_seed = main.generate_server_seed(server_seed)
+                    client_seed, changeSeed = main.generate_client_seed(client_seed, changeSeed)
+                    roll, server_seed, client_seed = main.get_roll_and_seeds(nonce, server_seed, client_seed)
+                    connection.sendall(f"Client seed: {client_seed}\nNonce: {nonce}\n".encode())
+                    # print(f'\nRoll for nonce {nonce} is {roll}')data = connection.recv(BYTE_RECV).decode().strip()
+                    status = False
+                    f.write("Client Seed "+client_seed + "|\n")
+                    f.write("Server Seed " +server_seed + "|\n")
+                    #f.write("Nounce " +nonce + "|\n")
+                    data = connection.recv(BYTE_RECV).decode().strip()
+                    if data =="heads" or data =="tails":
+                        client_answer = data
+                        
+                    if roll <= 50:
+                        server_answer = "heads"
+                        connection.sendall("The result of the coin flip is heads!\n".encode())
+                        print("The result of the coin flip is heads!\n")
+                    else:
+                        server_answer = "tails"
+                        connection.sendall("The result of the coin flip is tails!\n".encode())
+                        print("The result of the coin flip is tails!\n")
+                    print(f"Server answer:{server_answer}")
+                    print(f"Client answer:{client_answer}")
+                    f.write("Client Answer "+client_answer +"|\n")
+                    f.write("Server Answer "+server_answer +"|\n")
+                    if client_answer != server_answer:
+                        connection.sendall("player have lost".encode())
+                        # print("player have lost")
+                    else:
+                        connection.sendall("player have won".encode())
                     
-                if roll <= 50:
-                    server_answer = "heads"
-                    connection.sendall("The result of the coin flip is heads!\n".encode())
-                    print("The result of the coin flip is heads!\n")
-                else:
-                    server_answer = "tails"
-                    connection.sendall("The result of the coin flip is tails!\n".encode())
-                    print("The result of the coin flip is tails!\n")
-                print(f"Server answer:{server_answer}")
-                print(f"Client answer:{client_answer}")
-                if client_answer != server_answer:
-                    connection.sendall("player have lost".encode())
-                    # print("player have lost")
-                else:
-                    connection.sendall("player have won".encode())
-                
-                    # print("The result of the coin flip is tails!\n")
-    #          Append variables to text file
-    #          Need to store client seed, server seed, nonce, roll, result.
-            elif data == "2":
-                pass
-            elif data == "3":
-                changeSeed = "User want to change"
-                data = connection.recv(BYTE_RECV).decode().strip()
-                print(data)
-                client_seed = data
-                # client_seed = main.generate_client_seed(client_seed, changeSeed)[0]
-                changeSeed = "User has changed"
-    return
+                        # print("The result of the coin flip is tails!\n")
+        #          Append variables to text file
+        #          Need to store client seed, server seed, nonce, roll, result.
+                elif data == "2":
+                    pass
+                elif data == "3":
+                    changeSeed = "User want to change"
+                    data = connection.recv(BYTE_RECV).decode().strip()
+                    print(data)
+                    client_seed = data
+                    # client_seed = main.generate_client_seed(client_seed, changeSeed)[0]
+                    changeSeed = "User has changed"
+        return
 
 try:
     while True:
@@ -110,11 +115,5 @@ except KeyboardInterrupt:
     sock.close()
     sys.exit(0)
 
-<<<<<<< Updated upstream
 print("[STARTING] server is starting...")
 
-
-=======
-print("[STARTING] server is starting...")
-
->>>>>>> Stashed changes
